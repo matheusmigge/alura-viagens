796da4b57caf17a3f955784b26e3e17245e27623
Matheus Migge <matheusmigge@gmail.com>
2023-09-28 16:57:52 -0300

    Initial Commit

---
+++ alura-viagens/alura_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagensApp.swift
+//  alura-viagens
+//
+//  Created by Migge on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
